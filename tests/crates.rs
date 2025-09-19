7d8f922ddfe817af68ed55d1f03e87ae0e4defad
Kelvin Modesto <km.modesto@gmail.com>
2025-09-19 16:10:21 -0300

    refactor: break lines to better understanding

--- tests/crates.rs
+++ tests/crates.rs
@@ -3,7 +3,6 @@ use reqwest::blocking::Client;
 use rocket::form::validate::Contains;
 use rocket::serde::json::serde_json::json;
 use serde_json::Value;
-
 pub mod common;
 
 #[test]
@@ -12,10 +11,7 @@ fn test_get_rustaceans() {
     let rustacean1: Value = common::create_test_rustacean(&client);
     let rustacean2: Value = common::create_test_rustacean(&client);
 
-    let response = client
-        .get("http://127.0.0.1:8000/rustaceans")
-        .send()
-        .unwrap();
+    let response = client.get("http://127.0.0.1:8000/crates").send().unwrap();
     assert_eq!(response.status(), StatusCode::OK);
 
     let json: Value = response.json().unwrap();
@@ -30,7 +26,7 @@ fn test_get_rustaceans() {
 fn test_create_rustacean() {
     let client = Client::new();
     let response = client
-        .post("http://127.0.0.1:8000/rustaceans")
+        .post("http://127.0.0.1:8000/crates")
         .json(&json!({ "name": "foo bar", "email": "foo@bar.com" }))
         .send()
         .unwrap();
@@ -49,22 +45,16 @@ fn test_create_rustacean() {
 fn test_view_rustacean() {
     let client = Client::new();
     let rustacean: Value = common::create_test_rustacean(&client);
-
     let response = client
-        .get(format!(
-            "http://127.0.0.1:8000/rustaceans/{}",
-            rustacean["id"]
-        ))
+        .get(format!("http://127.0.0.1:8000/crates/{}", rustacean["id"]))
         .send()
         .unwrap();
     assert_eq!(response.status(), StatusCode::OK);
-
     let rustacean: Value = response.json().unwrap();
     assert_eq!(
         rustacean,
         json!({ "id": rustacean["id"],"name": "Monkey D. Luffy", "email": "luffy@mugiwara.d", "created_at": rustacean["created_at"] })
     );
-
     common::delete_test_rustacean(&client, rustacean);
 }
 
@@ -72,23 +62,17 @@ fn test_view_rustacean() {
 fn test_update_rustacean() {
     let client = Client::new();
     let rustacean: Value = common::create_test_rustacean(&client);
-
     let response = client
-        .put(format!(
-            "http://127.0.0.1:8000/rustaceans/{}",
-            rustacean["id"]
-        ))
+        .put(format!("http://127.0.0.1:8000/crates/{}", rustacean["id"]))
         .json(&json!({ "name": "Monkey D. Luffy", "email": "nika@mugiwara.d" }))
         .send()
         .unwrap();
     assert_eq!(response.status(), StatusCode::OK);
-
     let rustacean: Value = response.json().unwrap();
     assert_eq!(
         rustacean,
         json!({ "id": rustacean["id"],"name": "Monkey D. Luffy", "email": "nika@mugiwara.d", "created_at": rustacean["created_at"] })
     );
-
     common::delete_test_rustacean(&client, rustacean);
 }
 
@@ -96,7 +80,6 @@ fn test_update_rustacean() {
 fn test_delete_rustacean() {
     let client = Client::new();
     let rustacean: Value = common::create_test_rustacean(&client);
-
     let response = client
         .delete(format!(
             "http://127.0.0.1:8000/rustaceans/{}",
