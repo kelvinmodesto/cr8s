9875f5572d9dabb5b94609771c1d3c56cbbfb3ad
Kelvin Modesto <km.modesto@gmail.com>
2025-09-26 17:29:35 -0300

    refactor(cli): add role on create cli

--- src/utils/commands.rs
+++ src/utils/commands.rs
@@ -1,6 +1,7 @@
 use diesel_async::{AsyncConnection, AsyncPgConnection};
 
-use crate::{models::NewUser, repositories::UserRepository};
+use crate::models::{NewRole, NewUser};
+use crate::repositories::{RoleRepository, UserRepository};
 
 async fn load_db_connection() -> AsyncPgConnection {
     let database_url =
@@ -14,7 +15,15 @@ pub async fn create_user(username: String, password: String, role_codes: Vec<Str
     let mut connection = load_db_connection().await;
     let new_user = NewUser { username, password };
 
-    let user = UserRepository::create(&mut connection, new_user);
+    let user = UserRepository::create(&mut connection, new_user, role_codes)
+        .await
+        .unwrap();
+    println!("User created {:?}", user);
+
+    let roles = RoleRepository::find_by_user(&mut connection, user)
+        .await
+        .unwrap();
+    println!("Roles assigned {:?}", roles);
 }
 
 pub async fn list_users() {
