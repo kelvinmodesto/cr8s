581dcd290e9399cc7f75f6a6b0fad8e07fc1bbb5
Kelvin Modesto <km.modesto@gmail.com>
2025-09-30 16:20:32 -0300

    feat(cache): add cache control into login endpoint

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,22 +1,32 @@
+use crate::config::{CacheConn, PgConn};
+use crate::repositories::UserRepository;
 use crate::utils::auth::{Credentials, authorize_user};
 use crate::utils::error::server_error;
-use crate::{config::postgres::PgConn, repositories::UserRepository};
+use rocket::http::Status;
 use rocket::response::status::Custom;
 use rocket::serde::json::{Json, Value, json};
 use rocket_db_pools::Connection;
+use rocket_db_pools::deadpool_redis::redis::AsyncCommands;
 
+//TODO: Put cache logic into a repository(aka sessionRepository)
 #[rocket::post("/login", format = "json", data = "<credentials>")]
 pub async fn login(
     mut db: Connection<PgConn>,
+    mut cache: Connection<CacheConn>,
     credentials: Json<Credentials>,
 ) -> Result<Value, Custom<Value>> {
-    UserRepository::find_by_username(&mut db, &credentials.username)
+    let user = UserRepository::find_by_username(&mut db, &credentials.username)
         .await
-        .map(|user| {
-            if let Ok(token) = authorize_user(&user, credentials.into_inner()) {
-                return json!(token);
-            }
-            json!("Unauthorized")
-        })
-        .map_err(|e| server_error(e.into()))
+        .map_err(|e| server_error(e.into()))?;
+    let session_id = authorize_user(&user, credentials.into_inner())
+        .map_err(|_| Custom(Status::Unauthorized, json!("Wrong credentials")))?;
+
+    cache
+        .set_ex::<String, i32, ()>(format!("sessions/{}", session_id), user.id, 3 * 60 * 60)
+        .await
+        .map_err(|e| server_error(e.into()))?;
+
+    Ok(json!({
+        "token": session_id,
+    }))
 }
