093df11dba70d2dfbe5aaf605d00ed2bf528965b
Kelvin Modesto <km.modesto@gmail.com>
2025-10-02 14:28:53 -0300

    test(common): create get_client methods with different roles

---
+++ tests/common/utils.rs
@@ -0,0 +1,100 @@
+use reqwest::StatusCode;
+use reqwest::blocking::{Client, ClientBuilder};
+use reqwest::header;
+use serde_json::{Value, json};
+use std::process::Command;
+
+pub static APP_HOST: &str = "http://127.0.0.1:8000";
+
+pub fn create_test_rustacean(client: &Client) -> Value {
+    let response = client
+        .post(format!("{}/rustaceans", APP_HOST))
+        .json(&json!({ "name": "Monkey D. Luffy", "email": "luffy@mugiwara.d" }))
+        .send()
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    response.json().unwrap()
+}
+
+pub fn create_test_crate(client: &Client, rustacean: &Value) -> Value {
+    let response = client
+        .post(format!("{}/crates", APP_HOST))
+        .json(&json!({
+            "name": "Thousand Sunny",
+            "code": "mugiwara",
+            "version": "0.10.3",
+            "description": "A crew of pirates that use to free people around the world" ,
+            "rustacean_id": rustacean["id"]
+        }))
+        .send()
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    response.json().unwrap()
+}
+
+pub fn delete_test_rustacean(client: &Client, rustacean: Value) {
+    let response = client
+        .delete(format!("{}/rustaceans/{}", APP_HOST, rustacean["id"]))
+        .send()
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::NO_CONTENT);
+}
+
+pub fn delete_test_crate(client: &Client, a_crate: Value) {
+    let response = client
+        .delete(format!("{}/crates/{}", APP_HOST, a_crate["id"]))
+        .send()
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::NO_CONTENT);
+}
+
+pub fn get_logged_in_client(username: &str, role: &str) -> Client {
+    let _ = Command::new("cargo")
+        .arg("run")
+        .arg("--bin")
+        .arg("cli")
+        .arg("users")
+        .arg("create")
+        .arg(username)
+        .arg("1234")
+        .arg(role)
+        .output();
+
+    let client = Client::new();
+
+    let response = client
+        .post(format!("{}/login", APP_HOST))
+        .json(&json!({
+            "username": "test_admin",
+            "password": "1234",
+        }))
+        .send()
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let json: Value = response.json().unwrap();
+    assert!(json.get("token").is_some());
+    let header_value = format!("Bearer {}", json["token"].as_str().unwrap());
+
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        header::AUTHORIZATION,
+        header::HeaderValue::from_str(&header_value).unwrap(),
+    );
+
+    ClientBuilder::new()
+        .default_headers(headers)
+        .build()
+        .unwrap()
+}
+
+pub fn get_client_with_logged_in_viewer() -> Client {
+    get_logged_in_client("test_viewer", "viewer")
+}
+
+pub fn get_client_with_logged_in_admin() -> Client {
+    get_logged_in_client("test_admin", "admin")
+}
