d1286e9e0e22565498be33054a32bd37bb81b127
Kelvin Modesto <km.modesto@gmail.com>
2025-10-03 15:18:28 -0300

    tests: Finish error tests cases for rustaceans

--- tests/rustaceans.rs
+++ tests/rustaceans.rs
@@ -133,11 +133,12 @@ mod rustaceans_error_cases {
     use super::*;
 
     #[test]
-    fn test_viewer_cannot_get_rustaceans() {
+    fn test_viewer_cannot_get_rustaceans_unauthorized() {
         let client = Client::new();
+        let client_admin = common::get_client_with_logged_in_admin();
 
-        let rustacean1: Value = common::create_test_rustacean(&client);
-        let rustacean2: Value = common::create_test_rustacean(&client);
+        let rustacean1: Value = common::create_test_rustacean(&client_admin);
+        let rustacean2: Value = common::create_test_rustacean(&client_admin);
 
         let response = client
             .get(format!("{}/rustaceans", common::APP_HOST))
@@ -145,33 +146,28 @@ mod rustaceans_error_cases {
             .unwrap();
         assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
 
-        common::delete_test_rustacean(&client, rustacean1);
-        common::delete_test_rustacean(&client, rustacean2);
+        common::delete_test_rustacean(&client_admin, rustacean1);
+        common::delete_test_rustacean(&client_admin, rustacean2);
     }
 
     #[test]
-    fn test_create_rustacean() {
-        let client = common::get_client_with_logged_in_editor();
+    fn test_create_rustacean_error() {
+        let client = Client::new();
+
         let response = client
             .post(format!("{}/rustaceans", common::APP_HOST))
             .json(&json!({ "name": "foo bar", "email": "foo@bar.com" }))
             .send()
             .unwrap();
-        assert_eq!(response.status(), StatusCode::CREATED);
-
-        let rustacean: Value = response.json().unwrap();
-        assert_eq!(
-            rustacean,
-            json!({ "id": rustacean["id"],"name": "foo bar", "email": "foo@bar.com", "created_at": rustacean["created_at"] })
-        );
-
-        common::delete_test_rustacean(&client, rustacean);
+        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
     }
 
     #[test]
     fn test_view_rustacean() {
-        let client = common::get_client_with_logged_in_editor();
-        let rustacean: Value = common::create_test_rustacean(&client);
+        let client = Client::new();
+        let client_admin = common::get_client_with_logged_in_admin();
+
+        let rustacean: Value = common::create_test_rustacean(&client_admin);
 
         let response = client
             .get(format!(
@@ -181,35 +177,31 @@ mod rustaceans_error_cases {
             ))
             .send()
             .unwrap();
-        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
 
-        let rustacean: Value = response.json().unwrap();
-        assert_eq!(
-            rustacean,
-            json!({ "id": rustacean["id"],"name": "Monkey D. Luffy", "email": "luffy@mugiwara.d", "created_at": rustacean["created_at"] })
-        );
-
-        common::delete_test_rustacean(&client, rustacean);
+        common::delete_test_rustacean(&client_admin, rustacean);
     }
 
     #[test]
-    fn test_view_rustacean_not_found() {
-        let client = common::get_client_with_logged_in_editor();
-        let rustacean: Value = common::create_test_rustacean(&client);
+    fn test_view_rustacean_unauthorized() {
+        let client = Client::new();
+        let client_editor = common::get_client_with_logged_in_editor();
+        let rustacean: Value = common::create_test_rustacean(&client_editor);
 
         let response = client
             .get(format!("{}/rustaceans/9999", common::APP_HOST))
             .send()
             .unwrap();
-        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+        assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
 
-        common::delete_test_rustacean(&client, rustacean);
+        common::delete_test_rustacean(&client_editor, rustacean);
     }
 
     #[test]
-    fn test_update_rustacean() {
-        let client = common::get_client_with_logged_in_editor();
-        let rustacean: Value = common::create_test_rustacean(&client);
+    fn test_update_rustacean_error() {
+        let client = Client::new();
+        let client_editor = common::get_client_with_logged_in_editor();
+        let rustacean: Value = common::create_test_rustacean(&client_editor);
 
         let response = client
             .put(format!(
@@ -220,21 +212,16 @@ mod rustaceans_error_cases {
             .json(&json!({ "name": "Monkey D. Luffy", "email": "nika@mugiwara.d" }))
             .send()
             .unwrap();
-        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
 
-        let rustacean: Value = response.json().unwrap();
-        assert_eq!(
-            rustacean,
-            json!({ "id": rustacean["id"],"name": "Monkey D. Luffy", "email": "nika@mugiwara.d", "created_at": rustacean["created_at"] })
-        );
-
-        common::delete_test_rustacean(&client, rustacean);
+        common::delete_test_rustacean(&client_editor, rustacean);
     }
 
     #[test]
-    fn test_delete_rustacean() {
-        let client = common::get_client_with_logged_in_editor();
-        let rustacean: Value = common::create_test_rustacean(&client);
+    fn test_delete_rustacean_error() {
+        let client = Client::new();
+        let client_editor = common::get_client_with_logged_in_editor();
+        let rustacean: Value = common::create_test_rustacean(&client_editor);
 
         let response = client
             .delete(format!(
@@ -244,6 +231,6 @@ mod rustaceans_error_cases {
             ))
             .send()
             .unwrap();
-        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
     }
 }
